970e5b4098fd7d21f79bb20b05656fcfbe198707
dickeyxxx <jeff@dickeyxxx.com>
2014-07-20 16:08:47 -0700

    simplified init

--- plaincache.go
+++ plaincache.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
-	addr, err := net.ResolveTCPAddr("tcp", ":3000")
-	if err != nil {
-		fmt.Printf("error resolving address: %s\n\n", err)
-		os.Exit(1)
-	}
-
-	server := NewServer()
 	httpServer := &http.Server{
-		Addr:         addr.String(),
-		Handler:      server.handler(),
+		Addr:         ":3000",
+		Handler:      NewServer().handler(),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	err = httpServer.ListenAndServe()
-	if err != nil {
-		fmt.Printf("fatal error while serving: %s", err)
-		os.Exit(1)
-	}
+	log.Fatal(httpServer.ListenAndServe())
 }
